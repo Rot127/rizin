1f70cea4f8fcaea06ffb7318e9d75adc54377f93
Rot127 <unisono@quyllur.org>
2022-03-30 12:12:29 -0400

    Ouline shufle constraints for il ops.

--- librz/analysis/arch/hexagon/il/hexagon_il.c
+++ librz/analysis/arch/hexagon/il/hexagon_il.c
@@ -1,23 +1,67 @@
+#include "hexagon.h"
+#include "hexagon_il.h"
+#include <rz_il.h>
+#include <rz_il/rz_il_opbuilder_begin.h>
 
-// RzAnalysisOp->op pointer to il operation.
-// This is noped for every instruction of a packet whci is *not* the last one.
-//
-// So:
-// - Disassemble instruction:
-//   - Not last -> Set il_op_nop -> Add ops to packet opt.
-//   - Last     -> Add ops to packet op -> Set il_packet_op
-//
-// il_op_pkt = RzIlEffect
-//
-// Read -> PURE
-// Exec -> PURE
-// Write -> EFFECT(PURE what, PURE where)
-// Jump -> EFFECT(PURE where)
-//
-//
-// Write examples
-// Rd = Rs                  - HEX_INS_A2_TFR
-// Rd = Rs + Rt             - HEX_INS_A2_ADD
-// Rd = memw(Rs+Ii)         - HEX_INS_L2_LOADRI_IO
-// jump Rs                  - HEX_INS_J2_JUMPR
-// if (Pv) memd(Rs+Ii)=Rtt  - HEX_INS_S2_PSTORERDT_IO
+/**
+ * \brief Returns the packet's instructions IL operations as sequence.
+ * The IL ops order does not necessarily match the asm instructions order!
+ *
+ * \param pkt The instruction packet.
+ * \return NOP if the packet is not valid (not fully disassembled).
+ * A sequence of all instructions IL operations.
+ */
+RZ_API RzILOpEffect *hex_get_pkt_il_ops(RZ_INOUT HexPkt *pkt) {
+  rz_return_val_if_fail(pkt, NOP);
+  if (!pkt->is_valid || !pkt->last_instr_present) {
+    return NOP;
+  }
+
+  // SLOT ORDER
+  //
+  // 0x0    slot3
+  // 0x4    slot2
+  // 0x8    slot1
+  // 0xa    slot0
+  // 
+  // ORDERING CONSTRAINTS
+  //
+  // General ordering constraints:                                                  [MANUAL v67 3.3.6]
+  //
+  // IL EXEC ORDERING CONSTRAINTS
+  // - [ ] Dual stores                                                              [MANUAL v67 5.4]
+  // - [ ] mem_noshuf from v65 onwards. Frist store then load.                      [MANUAL v67 5.5]
+  // - [ ] .new value store  insn class NV                                          [MANUAL v67 5.6]
+  // - [ ] .new predicates                                                          [MANUAL v67 6.1.4]
+  // - [ ] Double predicate write
+  // - [ ] Dual jumps                                                               [MANUAL v67 8.7]
+  // - [ ] .new value compare jumps                                                 [MANUAL v67 8.5]
+  //   - We need to separate the `p0 = bla` part from the `if` part.
+  //     Other insn could use p0.new.
+  // - [ ] Duplex instructions -> 28:16 slot 1 ; 13:0 slot 0                        [MANUAL v67 10.3]
+  // - [ ] Harware loops.
+  //
+  // DUAL STORES
+  //
+  // Go last. Order preserved.
+  // slot 1 then slot 0
+  //
+  // COMPARE JUMPS
+  //
+  // Compiler separates compare and jump into separated ops?
+  // Jump op to the end.
+  //
+  // Priority of op execution.
+  // 0. -> Highest priority -> Last in sequence.
+  // Order of instrucions.
+  // 
+  // 10. direct jumps
+  // 9.  cond. jumps (Pv.new and Pv)
+  // 8.  small writes same reg - memb(R3)
+  // 7.  large writes same reg - memw(R3)
+  // 6.  .new reads
+ 
+  return NULL;
+}
+
+#include <rz_il/rz_il_opbuilder_end.h>
